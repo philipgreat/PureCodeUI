8a11ac9a4b5a0b03a5c493ce9fc76b568f343810
philipgreat <philip_chang@163.com>
2016-07-15 23:46:12 +0800

    Initial Commit

---
+++ PureCodeUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PureCodeUI
+//
+//  Created by 张喜来 on 7/15/16.
+//  Copyright © 2016 张喜来. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
